acebb2ee2cd49f39f162186c0ee6ada7e4d4a24d
Liz Nelson <121459797+Lizzard08@users.noreply.github.com>
2023-07-14 13:31:11 -0600

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,33 @@
+<?php
+    // Create connection
+	$db = new SQLite3('coffeedb.sq3');
+	// Check connection
+    
+	if (!$db) {
+		die("Connection failed: " . $db->lastErrorMsg());
+	}
+
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+
+    $checkEmail = $db->querySingle("SELECT COUNT(*) as count FROM customers WHERE email_address ='$email'");
+
+    if ($checkEmail > 0){
+        echo "Email already exists in database.";
+    } else {
+        $query = "INSERT INTO customers (first_name, last_name, email_address) VALUES ('$fname', '$lname', '$email')";
+        $result = $db->exec($query);
+
+        if ($result){
+            echo "Data inesrted successfully.";
+        } else {
+            echo "Error inserting data: " . $db->lastErrorMsg();
+        }
+
+    }
+
+    $db->close();
+
+?>
+	
